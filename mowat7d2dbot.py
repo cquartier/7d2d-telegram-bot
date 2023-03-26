4dea97f02dc96bea6086c6cbd935012c704162be
Chris Quartier <cquartier@gmail.com>
2023-03-26 17:10:54 -0600

    add recurring job to poll server for time to alert when blood moon day has arrived

--- mowat7d2dbot.py
+++ mowat7d2dbot.py
@@ -10,9 +10,20 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
+    format='%(asctime)s [%(levelname)s]: %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S'
 )
+
+def get_servertime(ticks):
+    curServerDays = ticks / 24000
+    curServerDay = math.floor(curServerDays) + 1
+    curServerHours = (curServerDays - math.floor(curServerDays)) * 24
+    curServerHour = math.floor(curServerHours)
+    curServerMinutes = (curServerHours - math.floor(curServerHours)) * 60
+    curServerMinute = math.floor(curServerMinutes)
+    curServerTime = f"{curServerHour:02d}:{curServerMinute:02d}"
+    return (curServerDay, curServerTime)
 
 def get_info():
     return None
@@ -46,11 +57,13 @@ def get_rules(host, port):
 
     return dataDict
 
-def cmd_start(args):
+def cmd_start(args, active_chats):
 
     async def do_start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-        logging.info(context.args)
-        await context.bot.send_message(chat_id=update.effective_chat.id, text='\U0001f6a8 Is this thing on? \U0001f6a8')
+        if update.effective_chat.id not in active_chats:
+            active_chats[update.effective_chat.id] = {'blood_moon_alert': False}
+        logging.info(f"active_chats: {active_chats}")
+        await context.bot.send_message(chat_id=update.effective_chat.id, text='\U00002705 Starting Blood Moon Watch for this chat \U00002705')
 
     return do_start
 
@@ -58,28 +71,42 @@ def cmd_time(args):
 
     async def get_time(update: Update, context: ContextTypes.DEFAULT_TYPE):
         rules = get_rules(args.host, args.port)
+        if rules is None:
+            # nothing to do, the server didn't respond. Just return
+            return
 
         logging.debug(rules)
 
-        curServerTime = int(rules['CurrentServerTime'])
-
-        curServerDays = curServerTime / 24000
-        logging.info(curServerDays)
-
-        curServerDay = math.floor(curServerDays) + 1
-        logging.info(curServerDay)
+        day, time = get_servertime(int(rules['CurrentServerTime']))
         
-        curServerHours = (curServerDays - math.floor(curServerDays)) * 24
-        logging.info(curServerHours)
+        logging.info(f"day {day} {time}")
 
-        curServerHour = math.floor(curServerHours)
-        curServerMinutes = (curServerHours - math.floor(curServerHours)) * 60
-        curServerMinute = math.floor(curServerMinutes)
-        logging.info(f"day {curServerDay} {curServerHour:02d}:{curServerMinute:02d}")
-
-        await context.bot.send_message(chat_id=update.effective_chat.id, text=f"day {curServerDay} {curServerHour:02d}:{curServerMinute:02d}")
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=f"day {day} {time}")
 
     return get_time
+
+def job_alert_minute(args, active_chats):
+
+    async def callback_minute(context):
+        rules = get_rules(args.host, args.port)
+        if rules is None:
+            # nothing to do, the server didn't respond. Just return
+            return
+        ticks = int(rules['CurrentServerTime'])
+        logging.info(f"ticks {ticks}")
+        day, time = get_servertime(ticks)
+        logging.info(f"day {day} {time}")
+        logging.info(f"blood moon? {day % 7 == 0}")
+        if day % 7 == 0:
+            for cid in active_chats:
+                if not active_chats[cid]['blood_moon_alert']:
+                    await context.bot.send_message(chat_id=cid, text='\U0001f6a8 It is a Blood Moon! \U0001f6a8')
+                    active_chats[cid]['blood_moon_alert'] = True
+        else:
+            for cid in active_chats:
+                active_chats[cid]['blood_moon_alert'] = False
+
+    return callback_minute
 
 def main():
 
@@ -101,12 +128,16 @@ def main():
         help='telegram bot token'
     )
 
-    global args
     args = parser.parse_args()
 
+    active_chats = dict()
+
     app = ApplicationBuilder().token(args.token).build()
+    job_queue = app.job_queue
+
+    job_minute = job_queue.run_repeating(job_alert_minute(args, active_chats), interval=30, first=10)
     
-    start_handler = CommandHandler('start', cmd_start(args))
+    start_handler = CommandHandler('start', cmd_start(args, active_chats))
     #status_handler = CommandHandler('status', cmd_status)
     get_time_handler = CommandHandler('time', cmd_time(args))
     app.add_handler(start_handler)
