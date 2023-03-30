27b5b66a7ca93e598b75ecd904f89a14a9e5a2f3
Chris Quartier <cquartier@gmail.com>
2023-03-29 22:37:49 -0600

    midway through attempts to improve the bot. Need to move to different machine because WLS2 has issues with UDP fragmentation on current machine and the rules packets are not making it through

--- 7d2dtelebot.py
+++ 7d2dtelebot.py
@@ -6,6 +6,8 @@ import sys
 import math
 import json
 import argparse
+import a2s
+from decimal import *
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 
@@ -15,71 +17,95 @@ logging.basicConfig(
     datefmt='%Y-%m-%d %H:%M:%S'
 )
 
-def is_active_blood_moon(day, hour):
-    return((day % 7 == 0 and hour > 21) and (day % 7 == 1 and hour < 4))
+class ServerTime:
+    TICKS_PER_DAY = Decimal(24000)
+    def __init__(self, ticks):
+        self.ticks = Decimal(ticks)
+        self._days = self.ticks / ServerTime.TICKS_PER_DAY
+        self.days = math.floor(self._days) + 1
+        self._hours = (self._days % Decimal(1)) * Decimal(24)
+        self.hours = math.floor(self._hours)
+        self._minutes = (self._hours % Decimal(1)) * Decimal(60)
+        self.minutes = math.floor(self._minutes)
 
-def days_to_blood_moon(day):
-    return (7 - (day % 7))
+    def is_blood_moon_day(self):
+        return(self.days % 7 == 0)
 
-def get_servertime(ticks):
-    curServerDays = ticks / 24000
-    curServerDay = math.floor(curServerDays) + 1
-    curServerHours = (curServerDays - math.floor(curServerDays)) * 24
-    curServerHour = math.floor(curServerHours)
-    curServerMinutes = (curServerHours - math.floor(curServerHours)) * 60
-    curServerMinute = math.floor(curServerMinutes)
-    return (curServerDay, curServerHour, curServerMinute)
+    def is_active_blood_moon(self):
+        return((self.days % 7 == 0 and self.hours > 21) and (self.day % 7 == 1 and self.hours < 4))
 
-def get_info(host, port):
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    try:
-        packet = b'\xff\xff\xff\xff\x54Source Engine Query\x00'
-        logging.debug('sending info request')
-        s.sendto(packet, (host, port))
-        s.settimeout(10)
-        data, addr = s.recvfrom(128)
-        logging.debug(f"got data: {data}")
-        logging.debug('sending info request with challenge response')
-        s.sendto(packet + data[5:], (host, port))
-        s.settimeout(10)
-        data, addr = s.recvfrom(10240)
-        logging.debug(f"got data: {data}")
-    except socket.timeout:
-        logging.error("socket timeout on A2S_INFO request")
-        s.close()
-        return None
+class SevenDaysToDieServer:
 
-    s.close()
-    
-    # parse info
-    info = {
-        'protocol': int(data[5])
-    }
+    def __init__(self, host, port):
+        self._host = host
+        self._port = port
+        self.address = (host, port)
 
-    idx = 6
-    strEnd = data.find(b'\x00', idx)
-    info['Name'] = data[idx:strEnd].decode('utf-8')
-    idx = strEnd + 1
-    strEnd = data.find(b'\x00', idx)
-    info['Map'] = data[idx:strEnd].decode('utf-8')
-    idx = strEnd + 1
-    strEnd = data.find(b'\x00', idx)
-    info['Folder'] = data[idx:strEnd].decode('utf-8')
-    idx = strEnd + 1
-    strEnd = data.find(b'\x00', idx)
-    info['Game'] = data[idx:strEnd].decode('utf-8')
-    idx = strEnd + 1
-    info['ID'] = data[idx:(idx+2)]
-    idx = idx + 2
-    info['Players'] = int(data[idx])
-    idx = idx + 1
-    info['Max Players'] = int(data[idx])
+    def _get_rules(self):
+        rules = None
+        try:
+            rules = a2s.rules(self.address)
+        except socket.timeout:
+            logging.error("socket timeout on A2S_RULES request")
+        return rules
 
-    return info
+    async def job_alert_minute(self, context):
+        rules = self._get_rules()
 
+        if rules is None:
+            # nothing to do, the server didn't respond. Just return
+            return
 
-def get_players():
-    return None
+        stime = ServerTime(int(rules['CurrentServerTime']))
+
+        if stime.is_blood_moon_day():
+            for cid in active_chats:
+                if not active_chats[cid]['blood_moon']['day_alert']:
+                    await context.bot.send_message(chat_id=cid, text='\U0001f6a8 It is a Blood Moon day! \U0001f6a8')
+                    active_chats[cid]['blood_moon']['day_alert'] = True
+        else:
+            for cid in active_chats:
+                active_chats[cid]['blood_moon']['day_alert'] = False
+        
+        if stime.is_active_blood_moon():
+            for cid in active_chats:
+                if not active_chats[cid]['blood_moon']['start_alert']:
+                    await context.bot.send_message(chat_id=cid, text='\U0001f6a8\U0001f6a8\U0001f6a8 The Blood Moon has begun! \U0001f6a8\U0001f6a8\U0001f6a8')
+                    active_chats[cid]['blood_moon']['start_alert'] = True
+        else:
+            for cid in active_chats:
+                active_chats[cid]['blood_moon']['start_alert'] = False
+
+    async def cmd_start(self, update, context):
+        rules = get_rules(args.host, args.port)
+        info = get_info(args.host, args.port)
+        stime = ServerTime(rules['CurrentServerTime'])
+        if update.effective_chat.id not in active_chats:
+            data = {
+                'blood_moon': {
+                    'active': is_active_blood_moon(day, hour),
+                    'start_alert': False,
+                    'day_alert': False,
+                    'days_until': days_to_blood_moon(day)
+                },
+                'cur_day': day,
+                'cur_hour': hour
+            }
+            active_chats[update.effective_chat.id] = data
+            logging.info(f"active_chats: {active_chats}")
+            message = f"\U00002705 Starting Blood Moon Watch for this chat \U00002705\n\n"
+        else:
+            logging.info("this chat already active")
+            message = f"\U00002705 Blood Moon Watch already active for this chat \U00002705\n\n"
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+
+    async def cmd_status(self, update, context):
+        logging.info('in do_status callback...')
+        rules = self._get_rules()
+        if rules is None:
+            return
+        logging.info(f"{rules['Players']} out of {info['Max Players']} connected")
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=f"{info['Players']} out of {info['Max Players']} connected")
 
 def get_rules(host, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -109,40 +135,10 @@ def get_rules(host, port):
     return dataDict
 
 def cmd_start(args, active_chats):
-    async def do_start(update, context):
-        rules = get_rules(args.host, args.port)
-        info = get_info(args.host, args.port)
-        day, hour, minute = get_servertime(int(rules['CurrentServerTime']))
-        if update.effective_chat.id not in active_chats:
-            data = {
-                'blood_moon': {
-                    'active': is_active_blood_moon(day, hour),
-                    'start_alert': False,
-                    'day_alert': False,
-                    'days_until': days_to_blood_moon(day)
-                },
-                'cur_day': day,
-                'cur_hour': hour
-            }
-            active_chats[update.effective_chat.id] = data
-            logging.info(f"active_chats: {active_chats}")
-            message = f"\U00002705 Starting Blood Moon Watch for this chat \U00002705\n\n"
-        else:
-            logging.info("this chat already active")
-            message = f"\U00002705 Blood Moon Watch already active for this chat \U00002705\n\n"
-        await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
-
-    return do_start
+    
 
 def cmd_status(args):
-    async def do_status(update, context):
-        logging.info('in do_status callback...')
-        info = get_info(args.host, args.port)
-        if info is None:
-            return
-        logging.info(f"{info['Players']} out of {info['Max Players']} connected")
-        await context.bot.send_message(chat_id=update.effective_chat.id, text=f"{info['Players']} out of {info['Max Players']} connected")
-    return do_status
+    
 
 def cmd_time(args):
     async def get_time(update, context):
@@ -159,36 +155,9 @@ def cmd_time(args):
         await context.bot.send_message(chat_id=update.effective_chat.id, text=f"day {day} {hour:02d}:{minute:02d} ({state})")
     return get_time
 
-def job_alert_minute(args, active_chats):
+def (args, active_chats):
 
-    async def callback_minute(context):
-        rules = get_rules(args.host, args.port)
-        info = get_info(args.host, args.port)
-        if rules is None or info is None:
-            # nothing to do, the server didn't respond. Just return
-            return
-        ticks = int(rules['CurrentServerTime'])
-        day, hour, minute = get_servertime(ticks)
-
-        if day % 7 == 0:
-            for cid in active_chats:
-                if not active_chats[cid]['blood_moon']['day_alert']:
-                    await context.bot.send_message(chat_id=cid, text='\U0001f6a8 It is a Blood Moon day! \U0001f6a8')
-                    active_chats[cid]['blood_moon']['day_alert'] = True
-        else:
-            for cid in active_chats:
-                active_chats[cid]['blood_moon']['day_alert'] = False
-        
-        if is_active_blood_moon(day, hour):
-            for cid in active_chats:
-                if not active_chats[cid]['blood_moon']['start_alert']:
-                    await context.bot.send_message(chat_id=cid, text='\U0001f6a8\U0001f6a8\U0001f6a8 The Blood Moon has begun! \U0001f6a8\U0001f6a8\U0001f6a8')
-                    active_chats[cid]['blood_moon']['start_alert'] = True
-        else:
-            for cid in active_chats:
-                active_chats[cid]['blood_moon']['start_alert'] = False
-
-    return callback_minute
+    
 
 def main():
 
